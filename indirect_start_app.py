48d9b600287d8b3f4de449b31cb80f2d3e329946
emil.kholod <emil.kholod@gmail.com>
2020-03-17 09:01:53 +0500

    Refactor
    No need in init_celery in indirect_start_app.py, because it inits in create_app.

--- indirect_start_app.py
+++ indirect_start_app.py
@@ -1,9 +1,7 @@
 from app import celery
-from app.celery_util import init_celery
 from app.factory import create_app
 
 app = create_app()
-init_celery(app, celery)
 
 if __name__ == '__main__':
     app.run()
